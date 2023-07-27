3cf6b0abb67b19a7fd069fb6e0aff9706cb997aa
dantezhu <dantezhu@qq.com>
2023-07-28 00:13:45 +0800

    简单实现是直接修改旋转方向。但是实际上，在不同的碰撞位置，可能不一定合适。

--- clank/assets/scripts/SwordController.ts
+++ clank/assets/scripts/SwordController.ts
@@ -1,4 +1,5 @@
 import { _decorator, Component, Node, Collider2D, Contact2DType, PhysicsSystem2D, IPhysics2DContact} from 'cc';
+import RotateAround from './RotateAround';
 const { ccclass, property } = _decorator;
 
 @ccclass('SwordController')
@@ -14,17 +15,25 @@ export class SwordController extends Component {
             collider.on(Contact2DType.POST_SOLVE, this.onPostSolve, this);
         }
 
+        // 下面也可以用，但是选择一种就行
         // 注册全局碰撞回调函数
-        if (PhysicsSystem2D.instance) {
-            PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
-            PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
-            PhysicsSystem2D.instance.on(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
-            PhysicsSystem2D.instance.on(Contact2DType.POST_SOLVE, this.onPostSolve, this);
-        }
+        // if (PhysicsSystem2D.instance) {
+        //     PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        //     PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        //     PhysicsSystem2D.instance.on(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
+        //     PhysicsSystem2D.instance.on(Contact2DType.POST_SOLVE, this.onPostSolve, this);
+        // }
     }
     onBeginContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
         // 只在两个碰撞体开始接触时被调用一次
         console.log('onBeginContact');
+        let around = this.getComponent(RotateAround);
+
+        console.log('around before:', around)
+
+        around.clockwise = !around.clockwise;
+
+        console.log('around after:', around)
     }
     onEndContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
         // 只在两个碰撞体结束接触时被调用一次
